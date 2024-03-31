b56e6cc517d7a5d5c2eb2857cc89c73f94c0be54
WonHyun <coy4728@naver.com>
2024-03-31 15:56:24 +0900

    ref: change requestPermissions method

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,18 @@ import 'package:oneplayer/pages/main_page.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<void> requestPermissions() async {
-  // 저장소 접근 권한 상태 확인
-  var status = await Permission.manageExternalStorage.status;
-  if (!status.isGranted) {
-    // 저장소 접근 권한 요청
-    await Permission.manageExternalStorage.request();
+  //TODO: implement seperate screen
+  if (Platform.isAndroid || Platform.isIOS) {
+    var status = await Permission.manageExternalStorage.status;
+    if (!status.isGranted) {
+      await Permission.manageExternalStorage.request();
+    }
   }
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isAndroid || Platform.isIOS) {
-    await requestPermissions();
-  }
+  await requestPermissions();
   runApp(const MainApp());
 }
 
